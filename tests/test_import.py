d3f86cc8cf91221543d7a119a17dde210f357c5c
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2022-10-24 14:25:33 +0800

    Add unit test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,4 +1,12 @@
-import treehole as hole
+def test_import():
+    import treehole
 
-print("Version:", hole.__version__)
-print("Modules", hole.__dict__)
+    assert treehole is not None
+    assert treehole.__version__ is not None
+    assert treehole.__doc__ is not None
+    assert treehole.TreeHoleClient is not None
+    assert treehole.Hole is not None
+    assert treehole.ListHole is not None
+    assert treehole.AttentionHole is not None
+    assert treehole.Comment is not None
+    assert treehole.GenericHole is not None
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,72 +1,90 @@
 from treehole import Hole, ListHole, AttentionHole, Comment, UserName
 
-data = {
-    "pid": "4216687",
-    "text": "请问怎么在门户里查自己的尸检结果呀",
-    "type": "text",
-    "timestamp": "1666248960",
-    "reply": "74",
-    "likenum": "108",
-    "extra": "0",
-    "url": "",
-    "tag": None,
-}
 
-print("Hole", Hole.from_data(data))
-
-data = {
-    "pid": "4225323",
+fake_hole = {
+    "pid": "463721894531",
     "hidden": "0",
-    "text": "计概辅导的学长一般是几年级的呀",
+    "text": "fake message",
     "type": "text",
-    "timestamp": "1666429825",
-    "reply": "1",
+    "timestamp": "546123785",
+    "reply": "0",
     "likenum": "1",
     "extra": "0",
     "url": "",
-    "hot": "1666429825",
-    "tag": None,
-}
-
-print("ListHole", ListHole.from_data(data))
-
-data = {
-    "pid": "4114903",
-    "text": "网盘崩了？上传极慢，加载极慢",
-    "type": "text",
-    "timestamp": "1664124157",
-    "reply": "5",
-    "extra": "0",
-    "url": "",
-    "likenum": "1",
+    "hidden": "0",
+    "hot": "546123785",
     "tag": None,
     "attention_tag": None,
 }
 
-print("AttentionHole", AttentionHole.from_data(data))
-
-data = {
-    "cid": "18898276",
-    "pid": "4224963",
-    "text": "[Alice] 统计大家今天呼吸了吗，呼吸的扣a，没呼吸的扣b",
-    "timestamp": "1666424757",
+fake_comment = {
+    "cid": "367219",
+    "pid": "5467123805",
+    "text": "[Alice] Hello world",
+    "timestamp": "64712381543",
     "anonymous": "1",
     "tag": None,
-    "islz": 0,
+    "islz": "0",
     "name": "Alice",
 }
 
-print("Comment", Comment.from_data(data))
 
-assert ("Angry Alice" in UserName) == True
-assert ("alice" in UserName) == True
-assert ("a_lice" in UserName) == False
-assert ("you win 1234" in UserName) == True
-assert ("you win a234" in UserName) == False
+def test_hole_from_data():
+    """Only for testing type convertion"""
+    hole = Hole.from_data(fake_hole)
+    assert hole is not None and isinstance(hole, Hole)
+    assert isinstance(hole.pid, int)
+    assert isinstance(hole.timestamp, int)
+    assert isinstance(hole.reply, int)
+    assert isinstance(hole.likenum, int)
+    assert isinstance(hole.extra, int)
 
-assert UserName["you win 1234"] == 1234
-assert UserName[1234] == "You Win 1234"
-assert UserName["angry alice"] == 26
-assert UserName[48] == "Angry Winnie"
-assert UserName[17] == "Richard"
-assert UserName["zach"] == 25
+
+def test_list_hole_from_data():
+    """Only for testing type convertion"""
+    hole = ListHole.from_data(fake_hole)
+    assert hole is not None and isinstance(hole, ListHole)
+    assert isinstance(hole.pid, int)
+    assert isinstance(hole.timestamp, int)
+    assert isinstance(hole.reply, int)
+    assert isinstance(hole.likenum, int)
+    assert isinstance(hole.extra, int)
+    assert isinstance(hole.hot, int)
+    assert isinstance(hole.hidden, int)
+
+
+def test_attention_hole_from_data():
+    """Only testing type convertion"""
+    hole = AttentionHole.from_data(fake_hole)
+    assert hole is not None and isinstance(hole, AttentionHole)
+    assert isinstance(hole.pid, int)
+    assert isinstance(hole.timestamp, int)
+    assert isinstance(hole.reply, int)
+    assert isinstance(hole.likenum, int)
+    assert isinstance(hole.extra, int)
+
+
+def test_comment_from_data():
+    """Only testing type convertion"""
+    comment = Comment.from_data(fake_comment)
+    assert comment is not None and isinstance(comment, Comment)
+    assert isinstance(comment.cid, int)
+    assert isinstance(comment.pid, int)
+    assert isinstance(comment.timestamp, int)
+    assert isinstance(comment.anonymous, int)
+    assert isinstance(comment.islz, int)
+
+
+def test_user_name():
+    assert "Angry Alice" in UserName
+    assert "alice" in UserName
+    assert "a_lice" not in UserName
+    assert "you win 1234" in UserName
+    assert "you win a234" not in UserName
+
+    assert UserName["you win 702"] == 702
+    assert UserName[1234] == "You Win 1234"
+    assert UserName["angry alice"] == 26
+    assert UserName[48] == "Angry Winnie"
+    assert UserName[701] == "Zombie Zach"
+    assert UserName["zach"] == 25
